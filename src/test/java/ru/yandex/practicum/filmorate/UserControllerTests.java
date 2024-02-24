0fb7b6ec7262b45e87a22720a88e1e181d184a6c
elena.pranova <elena.pranova@corp.mail.ru>
2024-02-24 12:49:21 +0300

    delete import

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTests.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
