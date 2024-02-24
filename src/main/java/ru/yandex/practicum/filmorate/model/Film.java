9d03b7b7aff01ab96201ec80b3d0eeb4b3b285fe
elena.pranova <elena.pranova@corp.mail.ru>
2024-02-24 13:33:15 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
@@ -28,6 +28,6 @@ public class Film {
     @NotNull
     private final LocalDate releaseDate;
 
-    @PositiveOrZero
+    @Positive
     private final long duration;
 }
