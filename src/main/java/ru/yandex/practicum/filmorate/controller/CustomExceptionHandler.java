eb826eb589dca358c0feeeff48081607077af429
elena.pranova <elena.pranova@corp.mail.ru>
2024-02-26 20:46:54 +0300

    fix exception handler

--- src/main/java/ru/yandex/practicum/filmorate/controller/CustomExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/CustomExceptionHandler.java
@@ -2,7 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,6 +26,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleBadRequest(Exception ex, WebRequest request) {
         log.error("Ошибка, что неправильный запрос.", ex);
         return ResponseEntity.badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
                 .body(
                         new ErrorData(
                                 String.format("Неправильный запрос. Ошибка %s", ex.getMessage())
@@ -32,11 +37,23 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {UserNotFoundException.class, FilmNotFoundException.class})
     protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
         log.error("Ошибка, что объект не найден.", ex);
-        return ResponseEntity.badRequest().body(
-                new ErrorData(
-                        String.format("Ошибка, что объект не найден. Ошибка %s", ex.getMessage())
-                )
-        );
+        return ResponseEntity.badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(
+                        new ErrorData(
+                                String.format("Ошибка, что объект не найден. Ошибка %s", ex.getMessage())
+                        )
+                );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        return handleBadRequest(ex, request);
     }
 
     @Data
